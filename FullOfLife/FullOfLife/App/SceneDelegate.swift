137d45256e8746bebcefd99c9aac8da4c1ae21f0
Ruslanios22 <ilgamovich@gmail.com>
2023-08-24 14:29:27 +0300

    Setup project

---
+++ FullOfLife/FullOfLife/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  FullOfLife
+//
+//  Created by Руслан Шигапов on 24.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(
+            rootViewController: MainViewController()
+        )
+    }
+}
+
